b91e1df5f370a5a0fa0195ff6c7675032204daf3
seonghyeon <pshandkyp@gmail.com>
2022-02-18 15:22:53 +0900

    test

---
+++ src/main/java/co/smartFarm/prj/HomeController.java
@@ -0,0 +1,44 @@
+package co.smartFarm.prj;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+<<<<<<< HEAD
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+=======
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+>>>>>>> 28e005a8128007c874314097e4658128bcce0b79
+		
+		return "home/home";
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<<<<<<< HEAD
+	<jsp:forward page="home.do" />
+
+</body>
+</html>
+=======
+	<jsp:forward page="home.do" /> 
+
+</body>
+</html>
+>>>>>>> 28e005a8128007c874314097e4658128bcce0b79
