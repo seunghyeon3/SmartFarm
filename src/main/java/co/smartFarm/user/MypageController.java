047319fd47a60f47c84271c5a91b87dca5fce7a6
kjs <ggoso@gmail.com>
2022-02-22 09:03:03 +0900

    '20220222'

--- src/main/java/co/smartFarm/user/MypageController.java
+++ src/main/java/co/smartFarm/user/MypageController.java
@@ -19,6 +19,36 @@ public class MypageController {
 	public String mypage() {
 		return "user/mypage";
 	}
+	
+	//nft 생성버튼누를시 사진생성,
+		@RequestMapping("/nftGeneration.do")
+		public String nftGeneration() {
+		System.out.println("테슽");
+			  try {
+				   BufferedImage image1 = ImageIO.read(new File("/resources/nft/Background/봄.png"));
+				   BufferedImage image2 = ImageIO.read(new File("/resources/nft/Body/곰 똑냥이.png"));
+				   BufferedImage image3 = ImageIO.read(new File("/resources/nft/Medal/금메달.png"));
+
+				   int width = image1.getWidth();
+				   int height = image1.getHeight();
+
+				   BufferedImage mergedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+				   Graphics2D graphics = (Graphics2D) mergedImage.getGraphics();
+
+				   graphics.setBackground(Color.WHITE);
+				   graphics.drawImage(image1, 0, 0, null);
+				   graphics.drawImage(image2, 0, 0, null);
+				   graphics.drawImage(image3, 0, 0, null);
+				   
+				  // ImageIO.write(mergedImage, "gif", new File("c:\\mergedImage.gif"));
+				   // ImageIO.write(mergedImage, "jpg", new File("c:\\mergedImage.jpg"));
+				    ImageIO.write(mergedImage, "png", new File("../resources/nft//mergedImage.png"));
+				  } catch (IOException ioe) {
+				   ioe.printStackTrace();
+				  }
+
+			return null;
+		}
 
 	
 	
