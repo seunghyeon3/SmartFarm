80f887ef5f3a6eddf80ae4026514246b7c7f525c
Seol Lee <yisseol@naver.com>
2022-03-14 15:06:19 +0900

    220314

--- src/main/java/co/smartFarm/kit/kitWeb/KitController.java
+++ src/main/java/co/smartFarm/kit/kitWeb/KitController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 import co.smartFarm.kit.kitService.KitService;
 import co.smartFarm.kit.kitService.KitVO;
@@ -21,14 +24,22 @@ public class KitController {
 	// 키트 전체조회
 	// 220302 PSH shoppingController -> kitController 구분 작업
 	@GetMapping("/kitShopList.do")
-	public String kitShopList(Model model, @Param("kitPrpos") String kitPrpos) {
+	public String kitShopList(Model model, @Param("kitPrpos") String kitPrpos, @Param("kitName") String kitName) {
+		if (kitPrpos != null) {
+			List<KitVO> list = kitDao.kitSelectList(kitPrpos);
 
-		List<KitVO> list = kitDao.kitSelectList(kitPrpos);
+			// System.out.println(list);
 
-		// System.out.println(list);
+			model.addAttribute("kitSelectList", list);
 
-		model.addAttribute("kitSelectList", list);
+			return "shopping/kitShopList";
 
+		} else if (kitName != null) {// 키트 이름 조회인 경우
+			List<KitVO> list = kitDao.kitSelectOne(kitName);
+			String gson = new Gson().toJson(list);
+			model.addAttribute("kitSelectList", list);
+			return "shopping/kitShopList";
+		}
 		return "shopping/kitShopList";
 	}
 
@@ -42,7 +53,16 @@ public class KitController {
 		model.addAttribute("kitSelectOne", kitVo);
 
 		kitDao.kitUpdateHit(Integer.parseInt(kit_no));
-		//return "redirect:/kitProductDetail.do?kit_no=" + kitVo.getKit_no();
+		// return "redirect:/kitProductDetail.do?kit_no=" + kitVo.getKit_no();
 		return "shopping/kitProductDetail";
 	}
+
+	// ===== 키트 검색하기 =====
+	@GetMapping(value = "/searchKit.do", produces = "application/text;charset=utf8")
+	@ResponseBody
+	public String searchKit(@Param("kit_name") String kit_name) {
+
+		return null;
+	}
+
 }
